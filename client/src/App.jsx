fd080dfa273f079e9b277ef6a1ac56cfe30cde8c
pallipe123 <238w1a12g7@vrsec.ac.in>
2026-04-16 16:28:02 +0530

    day 2 admin, add product

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Register from './components/Register'
+import Login from './components/login'
+import Addproduct from './components/Addproduct'
+import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
+
 export default function App() {
   return (
     <BrowserRouter>
     <Navbar/>
-    <Routes>
-      <Route path='/' element={<h1>Home</h1>} />
-      <Route path='/register' element={<Register/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path="/add-product" element={
+          <ProtectedRoute>
+            <Addproduct/>
+          </ProtectedRoute>
+        }/>
+
+      </Routes>
     </BrowserRouter>
   )
 }
